a1370d64a86b202e8c7e013d5e729a6a4cd0df17
Daniel Leal <dlleal@uc.cl>
2021-10-22 14:32:14 -0300

    Remove unnecessary onSuccess hook on script widget constructor

--- script/src/main.js
+++ script/src/main.js
@@ -2,13 +2,11 @@ import Widget from './widget';
 
 function createWidget({
   title = 'Sample Widget',
-  onSuccess = () => {},
   onEvent = () => {},
   onExit = () => {},
 }) {
   const widget = new Widget({
     title,
-    onSuccess,
     onEvent,
     onExit,
   });
